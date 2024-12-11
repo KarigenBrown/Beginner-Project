ec73d04d5e266176fe463646b9ab62c68a436bb1
KarigenBrown <karigenbrown@gmail.com>
2024-12-11 21:57:32 +0100

    +login register

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,35 @@
 import react from "react"
+import {BrowserRouter, Navigate, Route, Routes} from "react-router-dom";
+import Register from "./pages/Register.jsx";
+import ProtectedRoute from "./components/ProtectedRoute.jsx";
+import Home from "./pages/Home.jsx";
+import Login from "./pages/Login.jsx";
+import NotFound from "./pages/NotFound.jsx";
+
+function Logout() {
+    localStorage.clear()
+    return <Navigate to="/login"/>
+}
+
+function RegisterAndLogout() {
+    localStorage.clear()
+    return <Register/>
+}
 
 function App() {
     return (
-        <>
-
-        </>
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={
+                    <ProtectedRoute>
+                        <Home/>
+                    </ProtectedRoute>}/>
+                <Route path="/login" element={<Login/>}/>
+                <Route path="/logout" element={<Logout/>}/>
+                <Route path="/register" element={<RegisterAndLogout/>}/>
+                <Route path="*" element={<NotFound/>}/>
+            </Routes>
+        </BrowserRouter>
     )
 }
 
